ec7750d09806359caa997ab448f33286591910f7
Olivier Malige <omalige@gmail.com>
2022-11-11 13:54:21 +0100

    Feature: Added Daisy-ui plugin for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './styles/globals.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold  underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <button className="btn">Daisy UI button</button>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const daisyui = require('daisyui');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
