b78d5cda2c49f9faa4ab8d610aec2e5f1d332d87
Olivier malige <omalige@gmail.Com>
2022-11-16 20:27:46 +0100

    Feature: Added filter persistence by url

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import './styles/globals.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { PhantomsProvider } from './contexts/phantoms/phantoms.context';
 import { MainLayout } from './layouts';
 import { DashboardPage } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DashboardPage />,
+  },
+]);
 
 function App() {
   return (
     <PhantomsProvider>
       <MainLayout>
-        <DashboardPage />
+        <RouterProvider router={router} />
       </MainLayout>
     </PhantomsProvider>
   );
--- src/components/CategoriesFilter/CategoriesFilter.tsx
+++ src/components/CategoriesFilter/CategoriesFilter.tsx
@@ -1,13 +1,21 @@
-import { useContext, useMemo } from 'react';
+import { useContext, useEffect, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import { PhantomsContext } from '../../contexts/phantoms/phantoms.context';
 import { FilterButton } from '../FilterButton/FilterButton';
 
 const CategoriesFilter = () => {
   const phantomsContext = useContext(PhantomsContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const searchCategoriesParams = searchParams.get('categories');
+
+    phantomsContext?.dispatchSetCategoryFilter(searchCategoriesParams || '');
+  }, [searchParams]);
 
   const handleClick = (category: string, isActive: boolean) => {
-    phantomsContext?.dispatchSetCategoryFilter(isActive ? '' : category);
+    setSearchParams(isActive ? '' : { categories: category });
   };
 
   const categoriesFilterButtons = useMemo(
